65f14140ec28aecabffd20d07f18bf60ad3cff7c
Leonard <364452445@qq.com>
2016-03-23 23:05:38 +0800

    添加个transform扩展方法

--- UnityExtersion/TransformExtension.cs
+++ UnityExtersion/TransformExtension.cs
@@ -14,4 +14,16 @@ public static class TransformExtension {
         T component = t.GetComponent<T>();
         return component;
     }
+
+    public static void SetParentEx(this Transform transform, Transform parent, Vector3 localPosition, Quaternion rotation, Vector3 scale) {
+
+        if (transform != null) {
+            transform.SetParent(parent);
+            transform.localPosition = localPosition;
+            transform.localRotation = rotation;
+            transform.localScale = scale;
+        }
+    }
+
+
 }
