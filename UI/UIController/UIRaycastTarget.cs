4e3168523e46bdff48ca198db89316b836561f9d
Leonard <364452445@qq.com>
2016-04-08 23:36:51 +0800

    上传一个可以响应ugui事件但是不参与绘制的图形组建脚本

---
+++ UI/UIController/UIRaycastTarget.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIRaycastTarget : MaskableGraphic {
+
+    protected UIRaycastTarget() {
+        useLegacyMeshGeneration = false;
+    }
+
+    protected override void OnPopulateMesh(VertexHelper vh) {
+        vh.Clear();
+    }
+}
