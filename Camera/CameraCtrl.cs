4acc88ebefdbd3c75058037655b15ba577079a8c
Leonard <364452445@qq.com>
2016-04-10 10:35:08 +0800

    Update CameraCtrl

--- Camera/CameraCtrl.cs
+++ Camera/CameraCtrl.cs
@@ -7,93 +7,113 @@ public class CameraCtrl : MonoBehaviour {
         public Vector3 localEuler;
         public Vector3 localPos;
     }
-    public float smoothTime = 0.3f;
     public Transform FollowObj;
     public Transform Pivot;
     public Transform Zoom;
-    public float Duration;
-    public float Distance;
 
     [SerializeField]
     public CameraData camSetting;
 
+    bool dirty = false;
     float timer = 0f;
-
+    float t = 0f;
+    Vector3 posRef;
+    Vector3 rotRef;
     Vector3 beginEuler = Vector3.zero;
     Vector3 endEuler = Vector3.zero;
     Vector3 beginPos = Vector3.zero;
     Vector3 endPos = Vector3.zero;
 
+    [SerializeField]
+    private float m_Duration;
+    public float duration {
+        get {
+            return Mathf.Clamp(m_Duration, 0.01f, float.MaxValue);
+        }
+    }
+
+    [SerializeField]
+    private Camera m_Camera;
+    public Camera mCamera {
+        get {
+            return m_Camera;
+        }
+        set {
+            m_Camera = value;
+        }
+    }
+
     public float ZoomX {
         set {
             camSetting.localPos = new Vector3(value, camSetting.localPos.y, camSetting.localPos.z);
-            resetTarget = true;
+            dirty = true;
         }
     }
     public float ZoomY {
         set {
             camSetting.localPos = new Vector3(camSetting.localPos.x, value, camSetting.localPos.z);
-            resetTarget = true;
+            dirty = true;
         }
     }
     public float ZoomZ {
         set {
             camSetting.localPos = new Vector3(camSetting.localPos.x, camSetting.localPos.y, value);
-            resetTarget = true;
+            dirty = true;
         }
     }
 
     public float RotX {
         set {
             camSetting.localEuler = new Vector3(value, camSetting.localEuler.y, camSetting.localEuler.z);
-            resetTarget = true;
+            dirty = true;
         }
     }
     public float RotY {
         set {
             camSetting.localEuler = new Vector3(camSetting.localEuler.x, value, camSetting.localEuler.z);
-            resetTarget = true;
+            dirty = true;
         }
     }
     public float RotZ {
         set {
             camSetting.localEuler = new Vector3(camSetting.localEuler.x, camSetting.localEuler.y, value);
-            resetTarget = true;
+            dirty = true;
         }
     }
 
-    bool resetTarget = false;
-    float t = 0f;
+    private void OnEnable() {
+        dirty = true;
+    }
 
-    Vector3 posRef;
-    Vector3 rotRef;
-    private void Update() {
 
+    private void LateUpdate() {
         if (FollowObj != null) {
-            Vector3 newPos = Vector3.SmoothDamp(this.transform.position, FollowObj.position, ref posRef, smoothTime);
-            Vector3 newRot = Vector3.SmoothDamp(this.transform.eulerAngles, FollowObj.eulerAngles, ref rotRef, smoothTime);
-            this.transform.position = newPos;
-            this.transform.eulerAngles = newRot;
+            this.transform.position = FollowObj.position;
+            this.transform.rotation = FollowObj.rotation;
         }
-        else {
-            if (resetTarget) {
-                timer = Duration;
-                beginEuler = Pivot.localEulerAngles;
-                endEuler = camSetting.localEuler;
-                beginPos = Zoom.localPosition;
-                endPos = camSetting.localPos;
-                resetTarget = false;
-            }
 
-            if (timer > 0f) {
-                timer -= Time.deltaTime;
-                t = Mathf.Clamp01((Duration - timer) / Duration);
-                Pivot.localEulerAngles = Vector3.Lerp(beginEuler, endEuler, t);
-                Zoom.localPosition = Vector3.Lerp(beginPos, endPos, t);
-            }
+        if (dirty) {
+            timer = duration;
+            beginEuler = Pivot.localEulerAngles;
+            endEuler = camSetting.localEuler;
+            beginPos = Zoom.localPosition;
+            endPos = camSetting.localPos;
+            dirty = false;
+        }
+
+        if (timer > 0f) {
+            timer -= Time.deltaTime;
+            t = Mathf.Clamp01((duration - timer) / duration);
+            Pivot.localEulerAngles = Vector3.Lerp(beginEuler, endEuler, t);
+            Zoom.localPosition = Vector3.Lerp(beginPos, endPos, t);
         }
     }
 
 
-
+    void OnDrawGizmos() {
+        if (mCamera!=null) {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(mCamera.transform.position,0.3f);
+        }
+    }
 }
