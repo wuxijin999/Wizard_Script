ab5975ed020b2437e13544368340de83fe1beb1b
Leonard <364452445@qq.com>
2016-04-10 11:03:37 +0800

    add UIToolEditor update UIRoot.cs

---
+++ Editor/UIToolEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class UIToolEditor : Editor {
+
+
+
+    [MenuItem("UI/CreatUIRoot")]
+    public static void CreatUIRoot() {
+        GameObject go = Instantiate(AssetLoadTools.Load_UI("UIRoot"));
+        go.name = "UIRoot";
+    }
+}
--- UI/Root/UIRoot.cs
+++ UI/Root/UIRoot.cs
@@ -1,7 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
+[ExecuteInEditMode]
 public class UIRoot : SingletonMonobehavior<UIRoot> {
 
-
+    protected override void Awake() {
+        base.Awake();
+        if (EventSystem.current == null) {
+            GameObject go = Instantiate(AssetLoadTools.Load_UI("EventSystem"));
+            go.name = "EventSystem";
+            EventSystem.current = go.GetComponent<EventSystem>();
+        }
+    }
 }
