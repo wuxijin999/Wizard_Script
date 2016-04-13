c812a77cd1f189fb62f4168b5c5f7bb33945493b
Leonard <364452445@qq.com>
2016-04-13 23:34:58 +0800

    Add ActionCmd ;Add ActionCmdProcessor

---
+++ Fight/ActionCmd.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public enum CmdType {
+    MoveTo,
+    Stand,
+    CastSkill,
+    Defense,
+    Dance,
+}
+
+
+public class ActionCmd {
+
+    protected CmdType type;
+
+    protected bool mCompleteFlag;
+    public bool completeFlag {
+        get {
+            return mCompleteFlag;
+        }
+    }
+
+    public ActionCmd(CmdType _type) {
+        type = _type;
+    }
+
+    public virtual void Excute() {
+
+    }
+
+
+}
+
+
---
+++ Fight/ActionCmdProcessor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ActionCmdProcessor {
+
+    Queue<ActionCmd> commandQueue = new Queue<ActionCmd>();
+
+    ActionCmd actionCmd = null;
+    public void PuctCmd(ActionCmd _cmd) {
+        commandQueue.Enqueue(_cmd);
+    }
+
+
+    public void Excute() {
+        if (commandQueue.Count < 1) {
+            return;
+        }
+        actionCmd = commandQueue.Peek();
+        actionCmd.Excute();
+        if (actionCmd.completeFlag) {
+            commandQueue.Dequeue();
+        }
+    }
+
+
+    public void Stop() {
+
+    }
+
+
+
+
+
+
+}
