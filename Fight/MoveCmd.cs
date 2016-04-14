945f29aa59b19d2572ab762ed385d50441732f03
Leonard <364452445@qq.com>
2016-04-14 23:09:06 +0800

    Update MoveCmd

--- Fight/MoveCmd.cs
+++ Fight/MoveCmd.cs
@@ -4,16 +4,18 @@ using System.Collections;
 public class MoveCmd : ActionCmd {
 
     Vector3 targetPosition = Vector3.zero;
-
+    Transform transform = null;
     public MoveCmd(CmdType _type, Actor _owner, Vector3 _targetPosition)
         : base(_type, _owner) {
-
+        transform = _owner.transform;
         targetPosition = _targetPosition;
     }
 
     public override void Excute() {
 
-
+        if (Vector3.Distance(transform.position, targetPosition) < 0.1f) {
+            mCompleteFlag = true;
+        }
     }
 
 }
