9b349b27cb50b3122b6eb5b6c857c59e6a421d52
Leonard <364452445@qq.com>
2016-04-15 23:51:06 +0800

    Add CastSkillCmd.cs

---
+++ Fight/CastSkillCmd.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ActionCommand {
+    public class CastSkillCmd : ActionCmd {
+
+        public CastSkillCmd(CmdType _type, Actor _owner) :
+            base(_type, _owner) {
+
+        }
+
+        public override void Begin() {
+            base.Begin();
+        }
+
+        public override void Excute() {
+            base.Excute();
+        }
+
+        public override void End() {
+            base.End();
+        }
+
+    }
+}
+
