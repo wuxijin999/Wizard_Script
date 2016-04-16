706743f1a1642cbfe0bf16a6e9cde3b864fbc632
Leonard <364452445@qq.com>
2016-04-16 23:39:49 +0800

    small update

--- Fight/CastSkillCmd.cs
+++ Fight/CastSkillCmd.cs
@@ -11,10 +11,13 @@ namespace ActionCommand {
 
         public override void Begin() {
             base.Begin();
+            //让actor进入某种动作状态
         }
 
         public override void Excute() {
             base.Excute();
+            //判断动作状态，是否释放完成，如果是，则标记为complete
+
         }
 
         public override void End() {
