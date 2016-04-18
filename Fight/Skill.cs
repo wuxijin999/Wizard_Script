9cc6c798aa726670277eed6b0a346ca783136c07
Leonard <364452445@qq.com>
2016-04-18 23:48:37 +0800

    Add Skill

---
+++ Fight/Skill.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Skill {
+
+    int skillId = 0;
+    public int SkillId {
+        get {
+            return skillId;
+        }
+    }
+
+    int skillInstId = 0;
+    public int SkillInstId {
+        get {
+            return skillInstId;
+        }
+    }
+
+    public Skill(int _skillId) {
+        skillId = _skillId;
+        skillInstId = BattleManager.Instance.AllocateSkillInstanceId();
+    }
+
+
+}
