beab39d38ac825b5b1db99c7364f83b9a6f4a03d
Leonard <364452445@qq.com>
2016-04-18 23:49:29 +0800

    Add SkillManager ,Add SkillProcessor

---
+++ Fight/SkillManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkillManager {
+
+    int skillInstanceId = 1;
+
+    /// <summary>
+    /// 请求分配技能实例Id
+    /// </summary>
+    /// <returns></returns>
+    public int AllocateSkillInstanceId() {
+        skillInstanceId++;
+
+        return skillInstanceId;
+    }
+
+
+
+}
---
+++ Fight/SkillProcessor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkillProcessor {
+
+    Skill skill = null;
+    public SkillProcessor(Skill _skill) {
+        skill = _skill;
+
+
+    }
+
+    public void AttackJuge() {
+
+       //检索敌人
+       //向战斗管理器报告
+    }
+
+
+
+}
