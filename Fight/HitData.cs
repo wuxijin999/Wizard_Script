49f8981a631d4625beabd100b274f85d90216ce9
Leonard <364452445@qq.com>
2016-04-19 23:06:18 +0800

    Add HitData ,add HitDataPool

---
+++ Fight/HitData.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class HitData {
+
+    public struct DecisionSphere {
+        public Vector3 Center;
+        public float Radius;
+    }
+
+    public readonly int hitId;
+    public readonly DecisionSphere[] decisionSpheres;
+
+
+    public HitData() {
+
+    }
+
+}
---
+++ Fight/HitDataPool.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HitDataPool {
+
+    Dictionary<int, HitData> hitDataDict = null;
+
+    public HitDataPool() {
+        hitDataDict = new Dictionary<int, HitData>();
+    }
+
+    public HitData QueryHitData(int _hitId) {
+        HitData hitData = null;
+        if (hitDataDict.TryGetValue(_hitId, out hitData)) {
+            return hitData;
+        }
+        else {
+            //从本地加载一个
+            hitData = new HitData();
+            hitDataDict[_hitId] = hitData;
+            return hitData;
+        }
+
+    }
+}
