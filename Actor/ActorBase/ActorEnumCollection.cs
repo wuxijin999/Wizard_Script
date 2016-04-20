df72db0ff5115b0c828563594eabdaf183b94568
Leonard <364452445@qq.com>
2016-04-21 00:00:49 +0800

    Update ActorEnumCollection BodyState

--- Actor/ActorBase/ActorEnumCollection.cs
+++ Actor/ActorBase/ActorEnumCollection.cs
@@ -4,8 +4,20 @@ using System.Collections;
 namespace Fight {
 
     public enum BodyState {
-
+        Idle,
+        Walk,
+        Run,
+        Cast,
+        Stuck,                          //硬直
+        HitFly,                          //击飞
+        HitBack,                       //击退
+        HitDown,                     //击倒
+        Float,                           //浮空
+        Stun,                           //晕眩
+        Morph,                       //被变形
+        Dead,
     }
+
 
 
 }
