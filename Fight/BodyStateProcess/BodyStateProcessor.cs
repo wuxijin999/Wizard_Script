8d51428e2936fd9899d577f8600a090f6bd63b29
Leonard <364452445@qq.com>
2016-04-21 00:01:28 +0800

    Add BodyStateProcess add FloatStateProcessor

---
+++ Fight/BodyStateProcess/BodyStateProcessor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Fight {
+
+    public class BodyStateProcessor {
+
+        public void Begin() {
+
+        }
+
+        public void End() {
+
+        }
+
+        protected virtual void OnBegin() {
+
+        }
+
+        protected virtual void DoAction() {
+
+        }
+
+        protected virtual void OnEnd() {
+
+        }
+    }
+
+
+}
+
+
---
+++ Fight/BodyStateProcess/FloatStateProcessor.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Fight {
+
+    public class FloatStateProcessor : BodyStateProcessor {
+
+
+    }
+}
+
