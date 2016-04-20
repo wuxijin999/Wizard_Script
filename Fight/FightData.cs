68ffffc34a871b6873ed321ef73b199cffff2521
Leonard <364452445@qq.com>
2016-04-21 07:15:51 +0800

    update FightData

---
+++ Fight/FightData.cs
@@ -0,0 +1,33 @@
+﻿//------------------------------------------------------------
+//           存放Fight相关的数据结构
+//
+//------------------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+
+namespace Fight {
+
+    public class HitData {
+
+        public readonly int hitId;
+        public readonly DecisionSphere[] decisionSpheres;
+
+        public readonly FloatData floatData;
+
+        public HitData() {
+
+        }
+    }
+
+    public struct DecisionSphere {
+        public Vector3 Center;
+        public float Radius;
+    }
+
+    public struct FloatData {
+        public float Height;
+        public float RiseTime;
+    }
+
+}
