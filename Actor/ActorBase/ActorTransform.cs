da6f7ab81b40249b8c29ffe05607d50b388d1a00
Leonard <364452445@qq.com>
2016-04-23 23:37:28 +0800

    让地面高度同步变得平滑

--- Actor/ActorBase/ActorTransform.cs
+++ Actor/ActorBase/ActorTransform.cs
@@ -4,8 +4,10 @@ using System.Collections;
 public class ActorTransform : MonoBehaviour {
 
     CharacterController controller;
+    GroundHeightUpdateProcessor heightSynchronizer;
     protected virtual void Awake() {
         controller = this.GetComponent<CharacterController>();
+        heightSynchronizer = new GroundHeightUpdateProcessor(this.transform, controller.height * 0.5f);
     }
 
     protected virtual void OnEnable() {
@@ -21,7 +23,8 @@ public class ActorTransform : MonoBehaviour {
     }
 
     protected virtual void LateUpdate() {
-        UpdateHeight();
+        heightSynchronizer.MeasureHeight();
+        heightSynchronizer.UpdateHeight();
     }
 
     protected virtual void OnDisable() {
@@ -32,13 +35,39 @@ public class ActorTransform : MonoBehaviour {
 
     }
 
-    private void UpdateHeight() {
 
-        Ray ray = new Ray(this.transform.position.AddY(30), Vector3.down);
-        RaycastHit hit;
-        Physics.Raycast(ray, out hit, 50, Layer.GroundMask);
-        if (hit.collider != null) {
-            this.transform.position = this.transform.position.SetY(hit.point.y + controller.height * 0.5f);
+    public class GroundHeightUpdateProcessor {
+        Transform transform;
+        float offset = 0f;
+
+        float targetHeight = 0f;
+        float refHeight = 0f;
+        float smoothTime = 0.1f;
+
+        public GroundHeightUpdateProcessor(Transform _transform, float _offset) {
+            transform = _transform;
+            offset = _offset;
         }
+
+        public void MeasureHeight() {
+            Ray ray = new Ray(this.transform.position.AddY(30), Vector3.down);
+            RaycastHit hit;
+            Physics.Raycast(ray, out hit, 50, Layer.GroundMask);
+            if (hit.collider != null) {
+                targetHeight = hit.point.y + offset;
+            }
+        }
+
+        public void UpdateHeight() {
+            if (Mathf.Abs(transform.position.y - targetHeight) < 0.001f) {
+                return;
+            }
+
+            float newY = Mathf.SmoothDamp(transform.position.y, targetHeight, ref refHeight, smoothTime);
+            transform.position = transform.position.SetY(newY);
+        }
+
+
     }
+
 }
