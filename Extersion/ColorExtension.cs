90957d9f26a2ac3d39a69367bab2fcdd6adafe5d
Leonard <364452445@qq.com>
2016-04-24 23:29:25 +0800

    Add ColorExtension

---
+++ Extersion/ColorExtension.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class ColorExtension {
+
+    public static Color SetR(this Color color, float r) {
+        color.r = r;
+        return color;
+    }
+
+    public static Color SetG(this Color color, float g) {
+        color.g = g;
+        return color;
+    }
+
+    public static Color SetB(this Color color, float b) {
+        color.b = b;
+        return color;
+    }
+
+    public static Color SetA(this Color color, float a) {
+        color.a = a;
+        return color;
+    }
+
+
+}
