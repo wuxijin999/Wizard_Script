d5dcdf24065b4efd19a31423f6f1ca0b12383e99
Leonard <364452445@qq.com>
2016-04-27 10:16:58 +0800

    增加一个自定义log类型

---
+++ Tools/WDebug.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WDebug {
+
+    public static void Log(string _content) {
+        Debug.Log(_content);
+    }
+
+    public static void Waring(string _content) {
+        Debug.LogWarning(_content);
+    }
+
+    public static void Error(string _content) {
+        Debug.LogError(_content);
+    }
+}
