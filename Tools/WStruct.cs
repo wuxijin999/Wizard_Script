e8d8529ee7c7633e5315c78be661d5a591f3f3ac
Leonard <364452445@qq.com>
2016-04-27 10:17:49 +0800

    增加一种可以被clamp的整数封装

---
+++ Tools/WStruct.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class ClampInt {
+    int min;
+    int max;
+
+    private int mValue;
+    public int Value {
+        get {
+            return mValue;
+        }
+        set {
+            mValue = Mathf.Clamp(value, min, max);
+        }
+    }
+
+    public ClampInt(int _value, int _min = int.MinValue, int _max = int.MaxValue) {
+        mValue = _value;
+        min = _min;
+        max = _max;
+    }
+}
