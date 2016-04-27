eda640f595107c7490ce23c1c4234e500f403312
Leonard <364452445@qq.com>
2016-04-27 10:21:16 +0800

    增加一种可以被clamp的浮点数封装

--- Tools/WStruct.cs
+++ Tools/WStruct.cs
@@ -22,3 +22,25 @@ public class ClampInt {
         max = _max;
     }
 }
+
+
+    public class ClampFloat {
+        float min;
+        float max;
+
+        private float mValue;
+        public float Value {
+            get {
+                return mValue;
+            }
+            set {
+                mValue = Mathf.Clamp(value, min, max);
+            }
+        }
+
+        public ClampFloat(float _value, float _min = float.MinValue, float _max = float.MaxValue) {
+            mValue = _value;
+            min = _min;
+            max = _max;
+        }
+    }
