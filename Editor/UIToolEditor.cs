eff69afa6f5fc30e16d34865399c359ce3ec8fb5
Leonard <364452445@qq.com>
2016-04-27 11:07:39 +0800

    update UIToolEditor

--- Editor/UIToolEditor.cs
+++ Editor/UIToolEditor.cs
@@ -1,14 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEditor;
+using UnityEngine.UI;
 
 public class UIToolEditor : Editor {
 
 
 
     [MenuItem("UI/CreatUIRoot")]
-    public static void CreatUIRoot() {
+    public static void CreateUIRoot() {
         GameObject go = Instantiate(AssetLoadTools.Load_UI("UIRoot"));
         go.name = "UIRoot";
+    }
+
+
+    [MenuItem("UI/ButtonEmpty")]
+    public static void CreateEmptyButton() {
+        GameObject go = new GameObject();
+        go.AddMissComponent<RectTransform>();
+        UIRaycastTarget r = go.AddMissComponent<UIRaycastTarget>();
+        Button b = go.AddMissComponent<Button>();
+        b.transition = Selectable.Transition.None;
+        Canvas canvas = GameObject.FindObjectOfType<Canvas>();
+        if (canvas != null) {
+            go.transform.SetParentEx(canvas.transform, Vector3.zero, Quaternion.identity, Vector3.one);
+        }
+
+        go.name = "ButtonEmpty";
     }
 }
