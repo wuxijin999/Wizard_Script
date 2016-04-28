dfef37393acf8d8c731975a5128c3fb512791f09
Leonard <364452445@qq.com>
2016-04-28 23:32:57 +0800

    update PressTip

--- UI/UIController/PressTip.cs
+++ UI/UIController/PressTip.cs
@@ -8,9 +8,9 @@ public class PressTip : MonoBehaviour {
     RectTransform mRect;
 
     public void SetContent(string _content) {
+        Vector3 uiPos = CalculatePosition(Input.mousePosition);
 
-
-
+        this.transform.position = uiPos;
     }
 
     private Vector3 CalculatePosition(Vector3 _mousePosition) {
