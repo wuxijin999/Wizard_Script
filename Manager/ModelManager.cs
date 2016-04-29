b5d7b38ee3cde5fb18994892c2e5962d58bc0069
Leonard <364452445@qq.com>
2016-04-29 08:21:51 +0800

    Add ModelManager

---
+++ Manager/ModelManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace UI {
+
+    public abstract class ModelBase {
+
+        public void Init() {
+
+        }
+
+        protected void OnReconnected() {
+
+        }
+
+    }
+
+    public class ModelManager : Singleton<ModelManager> {
+
+        List<ModelBase> modelSet = null;
+
+
+        public void Init() {
+            modelSet = new List<ModelBase>();
+
+        }
+
+        public T GetModel<T>() where T : ModelBase {
+            T model = null;
+
+            for (int i = 0; i < modelSet.Count; i++) {
+                if (modelSet[i] is T) {
+                    model = modelSet[i] as T;
+                }
+            }
+
+            return model;
+        }
+
+
+
+    }
+
+
+}
+
