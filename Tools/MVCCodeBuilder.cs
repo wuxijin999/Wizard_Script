383fedf1b41c8b76dcaad719eb95a81faf263cdf
Leonard <364452445@qq.com>
2016-05-02 22:33:11 +0800

    add MVCCodeBuilder

---
+++ Tools/MVCCodeBuilder.cs
@@ -0,0 +1,32 @@
+﻿//--------------------------------------------------------
+//    [Author]:                   Wu Xijin
+//    [Date]   :           Monday, May 02, 2016
+//--------------------------------------------------------
+using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Text;
+
+public class MVCCodeBuilder : MonoBehaviour {
+
+    #region Fields
+    public TextAsset textAsset;
+    #endregion
+
+    #region Built-In
+    void Awake() {
+    }
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+    #endregion
+
+}
+
+
+
