56f052ce1c5c4cfdde28495765bb40f51855d428
wuxijin999 <364452445@qq.com>
2016-05-08 15:55:20 +0800

    update

--- Tools/Layer.cs
+++ Tools/Layer.cs
@@ -7,4 +7,10 @@ public class Layer {
     public static int GroundLayer = LayerMask.NameToLayer("Ground");
     public static int GroundMask = 1 << GroundLayer;
 
+    public static int UILayer = LayerMask.NameToLayer("UI");
+    public static int UIMask = 1 << UILayer;
+
+    public static int HUDLayer = LayerMask.NameToLayer("HUD");
+    public static int HUDMask = 1 << HUDLayer;
+
 }
