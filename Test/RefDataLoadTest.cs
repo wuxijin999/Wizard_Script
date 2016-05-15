9605f48acce97bea469f59f74777954cab37f157
wuxijin999 <364452445@qq.com>
2016-05-15 22:48:09 +0800

    新增测试脚本

---
+++ Test/RefDataLoadTest.cs
@@ -0,0 +1,31 @@
+﻿//--------------------------------------------------------
+//    [Author]:                   Wu Xijin
+//    [Date]   :           Sunday, May 15, 2016
+//--------------------------------------------------------
+using UnityEngine;
+using System.Collections;
+
+public class RefDataLoadTest : MonoBehaviour {
+
+    void Start () {
+        RefDataManager.Instance.Init();
+        RefHitData hitData = RefHitData.Get(1);
+
+        Debug.Log(
+             hitData.Id + "\r\n" +
+             hitData.AttackMode + "\r\n"
+            + hitData.AttackRange + "\r\n"
+            + hitData.TargetMode + "\r\n"
+            + hitData.TargetNum + "\r\n"
+            + hitData.TargetFaction + "\r\n"
+            + hitData.DamageType + "\r\n"
+            + hitData.DamageRate + "\r\n"
+            + hitData.DecisionCenter[0] + "\r\n"
+            + hitData.DecisionRadius[0] + "\r\n"
+            );
+    }
+
+}
+
+
+
