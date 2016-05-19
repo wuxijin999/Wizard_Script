3e80a40b8d0e451fb2b93f769e0f8994492100b2
wuxijin999 <364452445@qq.com>
2016-05-19 23:33:55 +0800

    添加状态机

---
+++ FSMState/FSMState.cs
@@ -0,0 +1,43 @@
+﻿//--------------------------------------------------------
+//    [Author]:                   Wu Xijin
+//    [Date]   :           Thursday, May 19, 2016
+//--------------------------------------------------------
+using UnityEngine;
+using System.Collections;
+
+
+
+public enum FSMSateStatus {
+    Inactive = 0,
+    Failure = 1,
+    Success = 2,
+    Running = 3
+}
+
+public class FSMState {
+
+    FSMStateMachine machine;
+
+    public FSMSateStatus Status {
+        get; private set;
+    }
+    public FSMState (FSMStateMachine _machine) {
+        machine = _machine;
+    }
+
+    public virtual void Enter () {
+        Status = FSMSateStatus.Running;
+    }
+
+    public virtual void Excute () {
+
+    }
+
+    public virtual void Exit () {
+
+    }
+
+}
+
+
+
---
+++ FSMState/FSMStateMachine.cs
@@ -0,0 +1,51 @@
+﻿//--------------------------------------------------------
+//    [Author]:                   Wu Xijin
+//    [Date]   :           Thursday, May 19, 2016
+//--------------------------------------------------------
+using UnityEngine;
+using System.Collections;
+
+public class FSMStateMachine {
+
+    FSMState currentState;
+    FSMState nextState;
+    FSMState defaultState;
+
+    public FSMStateMachine () {
+
+    }
+
+    public FSMState CreateState () {
+        FSMState state = new FSMState(this);
+        return state;
+    }
+
+    public void SetDefaultState (FSMState _defaultState) {
+        currentState = defaultState = _defaultState;
+        currentState.Enter();
+    }
+
+    public void FSMUpdate () {
+        if (currentState == null) {
+            return;
+        }
+
+        currentState.Excute();
+        if (currentState.Status == FSMSateStatus.Success) {
+            currentState.Exit();
+            currentState = null;
+            if (nextState != null) {
+                currentState = nextState;
+                nextState = null;
+            }
+            else {
+                currentState = defaultState;
+            }
+        }
+    }
+
+
+}
+
+
+
