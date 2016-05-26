2447685e736d538f60809b211b4b6f619e786229
wuxijin999 <364452445@qq.com>
2016-05-27 01:18:38 +0800

    提交特效播放工具类型

---
+++ Tools/SFXHelper.cs
@@ -0,0 +1,49 @@
+﻿//--------------------------------------------------------
+//    [Author]:                   Wu Xijin
+//    [Date]   :           Friday, May 27, 2016
+//--------------------------------------------------------
+using UnityEngine;
+using System.Collections;
+using System;
+
+
+public class EffectCtrl {
+
+    public Action onComplte = null;
+
+    private bool loop = false;
+    public bool Loop {
+        get {
+            return loop;
+        }
+        set {
+            loop = value;
+        }
+    }
+
+
+}
+
+public static class SFXExtersion {
+
+    static public EffectCtrl PlayEffect (this GameObject gameObject, string effectName) {
+        EffectCtrl effect = new EffectCtrl();
+
+        return effect;
+    }
+
+    static public EffectCtrl SetLoop (this EffectCtrl effect, bool _loop) {
+        effect.Loop = _loop;
+        return effect;
+    }
+
+    static public void OnComplete (this EffectCtrl effect, Action _callBack) {
+        effect.onComplte = _callBack;
+    }
+
+}
+
+
+
+
+
