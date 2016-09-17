081ddf4f91e2e8dcb1e581e8c28ccbf96a0c1711
wuxijin999 <364452445@qq.com>
2016-09-17 22:06:28 +0800

    Add UIPlayEffect

---
+++ Tools/UIPlayEffect.cs
@@ -0,0 +1,72 @@
+﻿//--------------------------------------------------------
+//    [Author]:                   Wu Xijin
+//    [Date]   :           Thursday, September 15, 2016
+//--------------------------------------------------------
+using UnityEngine;
+using System.Collections;
+
+public class UIPlayEffect : MonoBehaviour {
+
+    public enum AutoType {
+        None,
+        OnStart,
+        OnEnable,
+    }
+
+    #region Fields
+
+    private int m_EffectId;
+    public int effectId {
+        get {
+            return m_EffectId;
+        }
+        set {
+            m_EffectId = value;
+        }
+    }
+
+    [SerializeField]
+    private int m_Sortingorder;
+    public int sortingOrder {
+        get {
+            return m_Sortingorder;
+        }
+    }
+
+    private float m_Delay;
+    public float delay {
+        get {
+            return m_Delay;
+        }
+        set {
+            m_Delay = value;
+        }
+    }
+
+    private AutoType type;
+
+    #endregion
+
+    #region Built-In
+    void Awake () {
+
+    }
+
+    void Start () {
+
+    }
+
+    void Update () {
+
+    }
+
+
+    private void PlayEffect () {
+
+    }
+    #endregion
+
+}
+
+
+
