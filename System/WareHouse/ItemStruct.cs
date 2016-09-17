c932ce3ade84359f054f9405d61ac65c12751885
wuxijin999 <364452445@qq.com>
2016-09-17 22:03:51 +0800

    update ItemStruct and WareHouseModel

--- System/WareHouse/ItemStruct.cs
+++ System/WareHouse/ItemStruct.cs
@@ -9,18 +9,18 @@ namespace WizardSystem {
 
     public struct ItemEntry {
         public int Id;
-        public uint Num;
+        public int Num;
 
         public ItemEntry (int _id) {
             Id = _id;
             Num = 0;
         }
 
-        public void Add (uint _num) {
+        public void Add (int _num) {
             Num += _num;
         }
 
-        public void Del (uint _num) {
+        public void Del (int _num) {
             if (_num > Num) {
                 Num = 0;
             }
--- System/WareHouse/WareHouseModel.cs
+++ System/WareHouse/WareHouseModel.cs
@@ -12,6 +12,11 @@ namespace WizardSystem {
     public class WareHouseModel : ModelBase {
 
         #region Member
+        public delegate void ItemHandler (int _id);
+        public event ItemHandler ItemAddEvent;
+        public event ItemHandler ItemRemoveEvent;
+        public event ItemHandler ItemIncreaseEvent;
+        public event ItemHandler ItemDecreaseEvent;
 
         Dictionary<int, ItemEntry> itemDict;
         #endregion
@@ -24,37 +29,60 @@ namespace WizardSystem {
 
         }
 
-        public void Add (int _id, uint _num) {
+        public void Add (int _id, int _num) {
             ItemEntry entry;
 
             if (itemDict.ContainsKey(_id)) {
                 entry = itemDict[_id];
                 entry.Add(_num);
                 itemDict[_id] = entry;
+                if (ItemIncreaseEvent != null) {
+                    ItemIncreaseEvent(_id);
+                }
             }
             else {
                 entry = new ItemEntry(_id);
                 entry.Add(_num);
                 itemDict[_id] = entry;
+                if (ItemAddEvent != null) {
+                    ItemAddEvent(_id);
+                }
             }
 
         }
 
-        public void Del (int _id, uint _num) {
-            if (!itemDict.ContainsKey(_id)) {
-                WDebug.Log(string.Format("Can not find item of id :{0}", _id));
-                return;
+        public void Del (int _id, int _num) {
+            ItemEntry entry;
+            if (itemDict.TryGetValue(_id, out entry)) {
+                if (entry.Num > _num) {
+                    itemDict[_id].Del(_num);
+                    if (ItemDecreaseEvent != null) {
+                        ItemDecreaseEvent(_id);
+                    }
+                }
+                else {
+                    itemDict.Remove(_id);
+                    if (ItemRemoveEvent != null) {
+                        ItemRemoveEvent(_id);
+                    }
+                }
             }
-
-            itemDict[_id].Del(_num);
-
-            if (itemDict[_id].Num == 0) {
-                itemDict.Remove(_id);
+            else {
+                WDebug.Log(string.Format("Can not find item of id :{0}", _id));
             }
         }
 
         public int GetItemSum () {
             return itemDict.Count;
+        }
+
+        public bool TryGetItemEntry (int _id, out ItemEntry _itemEntry) {
+            if (itemDict.TryGetValue(_id, out _itemEntry)) {
+                return true;
+            }
+            else {
+                return false;
+            }
         }
 
         public List<int> GetItemList () {
