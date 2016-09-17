cb1b69922ee63ca0987aa4a09a14620e878205f1
wuxijin999 <364452445@qq.com>
2016-09-17 22:05:38 +0800

    Add HeroModel

---
+++ System/Hero/HeroModel.cs
@@ -0,0 +1,77 @@
+﻿//-----------------------------------------------------------------
+//         [Author]: Leonard.Wu 
+//         [Date]: Friday, September 16, 2016  
+//-----------------------------------------------------------------
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace WizardSystem {
+
+    public class HeroModel : ModelBase {
+
+        #region Member
+        Dictionary<int, HeroData> heroDataDict;
+        #endregion
+
+        public override void Init () {
+            heroDataDict = new Dictionary<int, HeroData>();
+        }
+
+        protected override void OnReconnected () {
+
+        }
+
+        public bool TryGetHeroData (int _id, out HeroData _data) {
+            if (heroDataDict.TryGetValue(_id, out _data)) {
+                return true;
+            }
+            else {
+                _data = new HeroData();
+                return false;
+            }
+        }
+
+        public bool TryGetHeroBasicData (int _id, out HeroBasicData _basicData) {
+            if (heroDataDict.ContainsKey(_id)) {
+                _basicData = heroDataDict[_id].basicData;
+                return true;
+            }
+            else {
+                _basicData = new HeroBasicData();
+                return false;
+            }
+        }
+
+        public bool TryGetHeroEquipData (int _id, out HeroEquipData _equipData) {
+            if (heroDataDict.ContainsKey(_id)) {
+                _equipData = heroDataDict[_id].equipData;
+                return true;
+            }
+            else {
+                _equipData = new HeroEquipData();
+                return false;
+            }
+        }
+
+        public bool TryGetHeroMainProperty (int _id, out HeroMainProperty _mainProperty) {
+            if (heroDataDict.ContainsKey(_id)) {
+                _mainProperty = heroDataDict[_id].mainProperty;
+                return true;
+            }
+            else {
+                _mainProperty = new HeroMainProperty();
+                return false;
+            }
+        }
+
+
+    }
+
+}
+
+
+
+
+
---
+++ System/Hero/HeroStruct.cs
@@ -0,0 +1,46 @@
+﻿//--------------------------------------------------------
+//    [Author]:                   Wu Xijin
+//    [Date]   :           Friday, September 16, 2016
+//--------------------------------------------------------
+using UnityEngine;
+using System.Collections;
+
+
+public struct HeroData {
+    public int id;
+    public bool isActive;
+    public HeroBasicData basicData;
+    public HeroEquipData equipData;
+    public HeroMainProperty mainProperty;
+}
+
+public struct HeroBasicData {
+    public int level;
+    public int starLevel;
+    public int rankLevel;
+    public int breakLevel;
+}
+
+
+public struct HeroEquipData {
+    public int weaponId;
+    public int helmetId;
+    public int breastId;
+    public int legId;
+    public int bootId;
+    public int gloveId;
+}
+
+public struct HeroMainProperty {
+    public int strength;
+    public int intelligence;
+
+}
+
+
+
+
+
+
+
+
