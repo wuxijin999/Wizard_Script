e6565a26d1791ec55b898e1396d352fc2af44ea3
wuxijin999 <364452445@qq.com>
2016-10-07 00:10:46 +0800

    add SmoothMask add UIGroup

---
+++ UI/Component/Other/UIGroup.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class UIGroup : MonoBehaviour, ICanvasRaycastFilter {
+
+    [Tooltip("Is Accept Raycast")]
+    [SerializeField]
+    private bool m_RaycastTarget = true;
+    public bool raycastTarget { get { return m_RaycastTarget; } set { m_RaycastTarget = value; } }
+
+    public bool IsRaycastLocationValid (Vector2 sp, Camera eventCamera) {
+        return raycastTarget;
+    }
+
+    public void SetActive (bool _active) {
+        this.gameObject.SetActive(_active);
+    }
+
+}
