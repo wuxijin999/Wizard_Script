249f6b867a2515c3652b42737856baa0631ca73b
wuxijin999 <364452445@qq.com>
2016-12-02 23:11:20 +0800

    更新smoothmask代码，支持多个材质

--- UI/Component/Decorate/SmoothMask.cs
+++ UI/Component/Decorate/SmoothMask.cs
@@ -22,9 +22,9 @@ public class SmoothMask : UIBehaviour, ICanvasRaycastFilter {
     private float m_SmoothVerticalRate;
 
     [SerializeField]
-    private Material m_ImageMat;
+    private Material[] m_ImageMatSet;
     [SerializeField]
-    private Material m_TextMat;
+    private Material[] m_TextMatSet;
 
     [Tooltip("It's  necessary")]
     [SerializeField]
@@ -51,12 +51,16 @@ public class SmoothMask : UIBehaviour, ICanvasRaycastFilter {
         leftBottom = m_Rect.TransformPoint(-m_Rect.rect.width * m_Rect.pivot.x, -m_Rect.rect.height * m_Rect.pivot.y, 0) / canvasScale.transform.localScale.x;
         rightTop = m_Rect.TransformPoint(m_Rect.rect.width * (1 - m_Rect.pivot.x), m_Rect.rect.height * (1 - m_Rect.pivot.y), 0) / canvasScale.transform.localScale.x;
 
-        if (m_ImageMat != null) {
-            P.SetSmoothMask(m_ImageMat, m_SmoothHorizontalRate, m_SmoothVerticalRate, new Vector4(leftBottom.x, leftBottom.y, rightTop.x, rightTop.y));
+        if (m_ImageMatSet != null) {
+            for (int i = 0; i < m_ImageMatSet.Length; i++) {
+                P.SetSmoothMask(m_ImageMatSet[i], m_SmoothHorizontalRate, m_SmoothVerticalRate, new Vector4(leftBottom.x, leftBottom.y, rightTop.x, rightTop.y));
+            }
         }
 
-        if (m_TextMat != null) {
-            P.SetSmoothMask(m_TextMat, m_SmoothHorizontalRate, m_SmoothVerticalRate, new Vector4(leftBottom.x, leftBottom.y, rightTop.x, rightTop.y));
+        if (m_TextMatSet != null) {
+            for (int i = 0; i < m_TextMatSet.Length; i++) {
+                P.SetSmoothMask(m_TextMatSet[i], m_SmoothHorizontalRate, m_SmoothVerticalRate, new Vector4(leftBottom.x, leftBottom.y, rightTop.x, rightTop.y));
+            }
         }
     }
 
